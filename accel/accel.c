349e4f5bb2b7c83f5e811cdfffb97fe4e1e35918
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2017-05-05 10:18:56 +0000

    projects:accel: Add Accelerometer interfacing project
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ accel/accel.c
@@ -0,0 +1,103 @@
+/* Accelerometer example 
+ * 
+ * I2C0--> Lcd
+ * I2C1--> Accelerometer
+ *
+ */
+
+#include "upm/jhd1313m1.hpp"
+#include "upm/mma7660.hpp"
+
+#define I2C_LCD_BUS 0
+#define I2C_ACCEL_BUS 1
+#define LCD_ADDR 0x3e
+#define RGB_ADDR 0x62
+#define ACCEL_ADDR 0x4c
+
+using namespace std;
+using namespace upm;
+
+Jhd1313m1 *lcd;
+MMA7660 *accel;
+
+void print_accel()
+{
+	float ax, ay, az;
+        char buffer[5];
+
+        /* get acceleration from sensor */
+        accel->getAcceleration(&ax, &ay, &az);
+
+        /* get x aceleration */
+        lcd->setCursor(1,0);
+        snprintf(buffer, sizeof(buffer), "%f", ax);
+        lcd->write(buffer);
+
+        /* get y acceleration */
+        lcd->setCursor(1,6);
+        snprintf(buffer, sizeof(buffer), "%f", ay);
+        lcd->write(buffer);
+
+        /* get z acceleration */
+        lcd->setCursor(1,12);
+        snprintf(buffer, sizeof(buffer), "%f", az);
+        lcd->write(buffer);
+}
+
+int main(void)
+{
+	int count;
+	uint8_t reg;
+	float ax, ay, az;
+	char buffer[5];
+
+	lcd = new upm::Jhd1313m1(I2C_LCD_BUS, LCD_ADDR, RGB_ADDR);
+	accel = new MMA7660(I2C_ACCEL_BUS, ACCEL_ADDR);
+
+	/* place device in standby mode so we can write registers */
+	accel->setModeStandby();
+
+	/* enable 64 samples per second */
+	accel->setSampleRate(MMA7660::AUTOSLEEP_64);
+
+	/* setup SR register */
+	accel->writeByte(MMA7660::REG_SR, 0x00);
+	
+	/* setup tap threshold */
+	accel->writeByte(MMA7660::REG_PDET, 0x44);
+
+	/* setup tap debounce */
+	accel->writeByte(MMA7660::REG_PD, 0x02);
+
+	/* place device into active mode */
+	accel->setModeActive();
+
+	/* bleed blue */
+	lcd->setColor(0x00, 0x00, 0xff);
+	lcd->setCursor(0,0);
+	lcd->write("X:");
+        lcd->setCursor(0,5);
+        lcd->write("Y:");
+        lcd->setCursor(0,12);
+        lcd->write("Z:");
+
+
+	while(1) {
+		/* change color to red if shake is detected*/
+		if (accel->tiltShake())
+			lcd->setColor(0xff, 0x00, 0x00);
+
+		/* change color to green if tap is detected*/
+	        if (accel->tiltTap())
+                	lcd->setColor(0x00, 0xff, 0x00);
+	
+		print_accel();
+	
+		/* sleep for 0.5s */
+		usleep(500000);
+
+		lcd->setColor(0x00, 0x00, 0xff);
+	}
+	return 0;
+}
+
